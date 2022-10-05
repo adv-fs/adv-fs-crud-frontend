cac6ce7cb42a04dffef3bd0ac32fd767a98fdb45
Brien Thomas <thomasbrien94@gmail.com>
2022-10-04 17:17:32 -0700

    auth-services functions for signUp, signIn, signOut, and verification of a user created

--- src/Services/auth-services.js
+++ src/Services/auth-services.js
@@ -1,4 +1,4 @@
-import { get, post, put, patch, del } from './fetch.js';
+import { get, post, del } from './fetch.js';
 
 const URL = '/api/v1/auth';
 
@@ -7,3 +7,22 @@ export async function signUp(credentials) {
   response.user = response.data;
   return response;
 }
+
+export async function signIn(credentials) {
+  const response = await post(`${URL}/signin`, credentials);
+  response.user = response.data;
+  return response;
+}
+
+export async function signOut() {
+  const response = await del(`${URL}/signout`);
+  return response;
+}
+
+export async function verifyUser() {
+  const response = await get(`${URL}/verify`);
+  response.user = response.data;
+  return response;
+}
+
+// local storing for users, not needed at this time
